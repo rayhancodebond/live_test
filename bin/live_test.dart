06632f15638ce2bc3ae141992f314c98b892e881
rayhancodebond <mrk45345@gmail.com>
2025-01-01 23:42:31 +0600

    Changes
    Changes

--- bin/live_test.dart
+++ bin/live_test.dart
@@ -1,16 +1,12 @@
-import 'dart:io';
-
 void main() {
-
   triangle();
 }
 
 void triangle() {
-  print('Please enter the Base  triangle:');
-  double base = double.parse(stdin.readLineSync()!);
+  double base = 56.0;
+  double height = 9.0;
 
-  print('Please enter the height  triangle:');
-  double height = double.parse(stdin.readLineSync()!);
   double area = 0.5 * base * height;
-  print('The area of the triangle is: $area');
+
+  print("The area of the triangle is: $area");
 }
